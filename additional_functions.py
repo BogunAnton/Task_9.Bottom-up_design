46f28c4c48791a33b14eb37c981eaed9d8d10e86
BogunAnton <bogunanton687@gmail.com>
2024-12-06 01:55:20 +0700

    Функция для ввода чисел во 2 задаче

--- additional_functions.py
+++ additional_functions.py
@@ -24,7 +24,19 @@ def input_large_number():
 
 def generate_random_number(length):
     # Генерация случайного большого числа заданной длины
-    number_str = ''.join([str(random.randint(0, 9)) for _ in range(length)])
+    #number_str = ''.join([str(random.randint(0, 9)) for _ in range(length)])
     # Преобразование строки в массив цифр
-    number_array = [int(digit) for digit in number_str]
+    number_array = [random.randint(0, 9) for _ in range(length)]
+    return number_array
+
+def input_number_array():
+    # Ввод строки, содержащей числа, разделенные пробелами
+    input_str = input("Введите числа, разделенные пробелами: ")
+    # Разделение строки на отдельные числа и преобразование их в целые числа
+    number_array = [int(number) for number in input_str.split()]
+    return number_array
+
+def generate_random_array(length):
+    # Генерация случайного массива чисел от 1 до 100 заданной длины
+    number_array = [random.randint(1, 100) for _ in range(length)]
     return number_array
--- main_functions.py
+++ main_functions.py
@@ -1,6 +1,6 @@
-from additional_functions import is_int, input_large_number, generate_random_number
+from additional_functions import *
 
-"""def input_two_mas(array1, array2):
+def input_two_mas(array1, array2):
     print("Выберите опцию 1-2:\n"
           "1. Ввести массивы самостоятельно\n"
           "2. Сгенерировать массивы случайным образом\n")
@@ -21,11 +21,9 @@ from additional_functions import is_int, input_large_number, generate_random_num
         print('error')
     print("Первый массив цифр:", array1)
     print("Второй массив цифр:", array2)
-    return True  # Возвращаем True, чтобы указать, что текст был введен"""
+    return True  # Возвращаем True, чтобы указать, что текст был введен
 
-def sum_or_difference_arrays():
-    array1 = [1, 2, 3, 4]
-    array2 = [5, 6, 7, 8]
+def sum_or_difference_arrays(array1, array2):
     print("Выберите находить сумму или разность массивов:\n"
           "1. Сумму\n"
           "2. Разность")
@@ -41,6 +39,33 @@ def sum_or_difference_arrays():
         sum2 = sum(number for number in array2)
         return sum1-sum2
 
+def input_two_numbers_mas(array1, array2):
+    print("Выберите опцию 1-2:\n"
+          "1. Ввести массивы самостоятельно\n"
+          "2. Сгенерировать массивы случайным образом\n")
+    option = input()
+    if is_int(option):
+        option = int(option)
+    if option == 1:
+        # Ввод двух больших чисел вручную
+        array1 = input_number_array()
+        array2 = input_number_array()
+    elif option == 2:
+        # Генерация двух больших чисел случайным образом
+        length1 = int(input("Введите количество цифр в случайном массиве: "))
+        array1 = generate_random_array(length1)
+        length2 = int(input("Введите количество цифр в случайном массиве: "))
+        array2 = generate_random_array(length2)
+    else:
+        print('error')
+    print("Первый массив:", array1)
+    print("Второй массив:", array2)
+    return True  # Возвращаем True, чтобы указать, что текст был введен
+
+
+
 if __name__ == "__main__":
-    result = sum_or_difference_arrays()
-    print(result)
+    array1 = []
+    array2 = []
+    input_two_numbers_mas(array1, array2)
+
